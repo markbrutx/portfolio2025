2f028757f1a21606fa4f22b0e36adfa75a53f037
MarkBrut <themarkbrut@gmail.com>
2024-12-06 21:32:29 +0500

    feat(analytics): enhance experience tracking metrics

--- src/app/components/apps/experience/experience.component.ts
+++ src/app/components/apps/experience/experience.component.ts
@@ -4,6 +4,8 @@ import { computed, signal } from '@angular/core';
 import { YEARS } from './constants/experience.constants';
 import { ExperienceService } from './services/experience.service';
 import ExperienceCardComponent from './components/experience-card/experience-card.component';
+import { AnalyticsService } from '../../../services/analytics.service';
+import { AnalyticsEvent } from '../../../constants/analytics.constants';
 
 @Component({
   selector: 'app-experience',
@@ -18,6 +20,7 @@ import ExperienceCardComponent from './components/experience-card/experience-car
 })
 export default class ExperienceComponent {
   private readonly experienceService = inject(ExperienceService);
+  private readonly analyticsService = inject(AnalyticsService);
   private readonly scrollPosition = signal(0);
 
   protected readonly experiences = this.experienceService.experiences;
@@ -33,6 +36,14 @@ export default class ExperienceComponent {
   }
 
   protected readonly toggleExperience = (id: string): void => {
+    const state = this.experienceService.experiences();
+    const isActive = state.experiences.some(exp => exp.id === id && exp.isActive);
+    
     this.experienceService.toggleExperience(id);
+    
+    this.analyticsService.trackUserInteraction(
+      !isActive ? AnalyticsEvent.EXPERIENCE_DETAILS_EXPANDED : AnalyticsEvent.EXPERIENCE_DETAILS_COLLAPSED,
+      { experienceId: id }
+    );
   };
 }
